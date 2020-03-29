eee97c2dd0776a959f018bb6655879431dad045d
RS7x <53720202+RS7x@users.noreply.github.com>
2020-03-29 06:50:47 +0100

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -2,7 +2,7 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 name 'Mythic Framework Notification System'
 author 'Alzar - https://github.com/Alzar'
-version 'v1.0.3'
+version 'v1.0.4'
 
 ui_page {
     'html/ui.html',
@@ -15,13 +15,20 @@ files {
 }
 
 client_scripts {
-	'client/main.lua'
+	'client/main.lua',
+	'client/depracated.lua'
 }
 
+exports {
+	'SendAlert',
+	'PersistentAlert',
+}
+
+--[[ WARNING: These are depracated and may be removed at a future date, Suggest not using it ]]
 exports {
 	'DoShortHudText',
 	'DoHudText',
 	'DoLongHudText',
 	'DoCustomHudText',
-	'PersistentHudText',
+	'PersistentHudText'
 }
