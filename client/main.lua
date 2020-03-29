9867a8abb147f54cf5012cf43550e335001991ad
RS7x <53720202+RS7x@users.noreply.github.com>
2020-03-29 06:48:31 +0100

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,41 +1,14 @@
 RegisterNetEvent('mythic_notify:client:SendAlert')
 AddEventHandler('mythic_notify:client:SendAlert', function(data)
-	DoCustomHudText(data.type, data.text, data.length, data.style)
+	SendAlert(data.type, data.text, data.length, data.style)
 end)
 
-RegisterNetEvent('mythic_notify:client:PersistentHudText')
-AddEventHandler('mythic_notify:client:PersistentHudText', function(data)
-	PersistentHudText(data.action, data.id, data.type, data.text, data.style)
+RegisterNetEvent('mythic_notify:client:PersistentAlert')
+AddEventHandler('mythic_notify:client:PersistentAlert', function(data)
+	PersistentAlert(data.action, data.id, data.type, data.text, data.style)
 end)
 
-function DoShortHudText(type, text, style)
-	SendNUIMessage({
-		type = type,
-		text = text,
-		length = 1000,
-		style = style
-	})
-end
-
-function DoHudText(type, text, style)
-	SendNUIMessage({
-		type = type,
-		text = text,
-		length = 2500,
-		style = style
-	})
-end
-
-function DoLongHudText(type, text, style)
-	SendNUIMessage({
-		type = type,
-		text = text,
-		length = 5000,
-		style = style
-	})
-end
-
-function DoCustomHudText(type, text, length, style)
+function SendAlert(type, text, length, style)
 	SendNUIMessage({
 		type = type,
 		text = text,
@@ -44,7 +17,7 @@ function DoCustomHudText(type, text, length, style)
 	})
 end
 
-function PersistentHudText(action, id, type, text, style)
+function PersistentAlert(action, id, type, text, style)
 	if action:upper() == 'START' then
 		SendNUIMessage({
 			persist = action,
